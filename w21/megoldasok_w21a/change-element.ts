ec7ae0ca94192be8adb768d234bc112954c5837e
Csokán Pál András <csokan.andras87@protonmail.ch>
2022-06-06 17:21:53 +0200

    Feladat megoldasok Jun 6 2022

--- w21/megoldasok_w21a/change-element.ts
+++ w21/megoldasok_w21a/change-element.ts
@@ -4,11 +4,13 @@
 
 let numbers:number[] = [1,2,3,8,5,6];
 
+console.log("Initial: " + numbers);
+
 let fourthElem = numbers.map(function(fourthElem){
     numbers.splice(3,1,4);
     return fourthElem;
 })
 
-console.log("Old array was [1,2,3,8,5,6]");
+//console.log("Old array was [1,2,3,8,5,6]");
 console.log("New array is: " + fourthElem);
 console.log("Number changed to: " + numbers[3]);
---
+++ w21/megoldasok_w21a/colors.ts
@@ -0,0 +1,17 @@
+'use strict';
+// -  Create a two dimensional list
+//    which can contain the different shades of specified colors
+// -  In `colors[0]` store the shades of green:
+//    `"lime", "forest green", "olive", "pale green", "spring green"`
+// -  In `colors[1]` store the shades of red:
+//    `"orange red", "red", "tomato"`
+// -  In `colors[2]` store the shades of pink:
+//    `"orchid", "violet", "pink", "hot pink"`
+
+let colors = [["lime", "forest green", "olive", "pale green", "spring green"],
+                ["orange red", "red", "tomato"],
+                ["orchid", "violet", "pink", "hot pink"]];
+
+console.log("Shades of green array: " + colors[0][3]);
+console.log("Shades of red array: " + colors[1][1]);
+console.log("Shades of pink array: " + colors[2][0]);
