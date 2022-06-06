6bbf13cd493554ea4dcb1cf8ba8364dca6636736
Csokán Pál András <csokan.andras87@protonmail.ch>
2022-06-06 21:44:31 +0200

    Megoldas Jun 6 2022

---
+++ w21/megoldasok_w21b/doubling.ts
@@ -0,0 +1,11 @@
+// -  Create a variable named `baseNumber` and assign the value `123` to it
+// -  Create a function called `doubleNumber()` that doubles it's input parameter
+//    and returns the doubled value
+// -  Log the result of `doubleNumber(baseNumber)` to the console
+let baseNumber:number = 123;
+
+function doubleNumber(num:number){
+    console.log(num*2);
+}
+
+doubleNumber(baseNumber);
