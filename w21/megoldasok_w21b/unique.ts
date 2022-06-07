bf2a0ecc57ee6c3ada41d1a7b827fb1377e7cdca
Csokán Pál András <csokan.andras87@protonmail.ch>
2022-06-07 21:05:04 +0200

    Unique megoldas

--- w21/megoldasok_w21b/unique.ts
+++ w21/megoldasok_w21b/unique.ts
@@ -1,6 +1,7 @@
 //  Create a function that takes a list of numbers as a parameter
 //  and returns a list of numbers where every number is unique (occurs only once)
-let arr = [1,34,66,87,34,1,96];
+
+let arr: number[] = [1,4,7,34,77,34,86,4];
 
 let findUniqueItems:number[] = arr.filter((v,i,a) => (a.indexOf(v)===i));
 
