527452966d8ce85b5d78e6a30686bb70ea2cfe09
Csokán Pál András <csokan.andras87@protonmail.ch>
2022-06-11 18:48:24 +0200

    Parameter Print

---
+++ w21/megoldasok_w21b/print-params.ts
@@ -0,0 +1,12 @@
+// -  Create a function called `printParams`
+//    which logs to the console the input parameters
+//    (can have multiple number of arguments)
+
+function printParams(params:any){
+    console.log(params);
+}
+
+printParams("BS");
+printParams("A Hello World");
+printParams(5);
+printParams(true);
