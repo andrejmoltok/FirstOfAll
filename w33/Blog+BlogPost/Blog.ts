397c769711252163d35bd2bc7423a289df5cd619
Csokán Pál András <csokan.andras87@protonmail.ch>
2022-06-29 18:52:54 +0200

    Delete functionban modositas sikertelen

--- w33/Blog+BlogPost/Blog.ts
+++ w33/Blog+BlogPost/Blog.ts
@@ -25,9 +25,10 @@ class Blog {
 
     public delete(index:number){
         if (index > 0 && index < this._blogposts.length-1){
-            // let temp = this._blogposts[index];
-            this._blogposts[index] = this._blogposts[0];
-            // this._blogposts[this._blogposts.length-1] = this._blogposts[this._blogposts.length-2];
+            let temp = this._blogposts[index];
+            for (let i=1; i<=this._blogposts.length-1; i++){
+            this._blogposts[index] = this._blogposts[index-i];            
+        }
             this._blogposts.shift();
         }
         else if (index == 0)
@@ -51,8 +52,9 @@ let blogger = new Blog();
 let first = new BlogPost('John Doe','Lorem ipsum','Lorem ipsum dolor sit amet.','2000.05.04.');
 let second = new BlogPost('Tim Urban','Wait but why','A popular long-form, stick-figure-illustrated blog about almost everything.','2010.10.10.');
 let third = new BlogPost('William Turton','One Engineer Is Trying to Get IBM to Reckon With Trump','Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention. When I asked to take his picture outside one of IBM’s New York City offices, he told me that he wasn’t really into the whole organizer profile thing.','2017.03.28.');
-let fourth = new BlogPost('Jack Brown','What"\'s" going on next?','The future is the present which keeps on evolving','2006.06.15');
+let fourth = new BlogPost('Update Post','Update Post','Update Post','Update Post');
 let fifth = new BlogPost('CikkIroNeve','CikkCime','CikkTartalma','CikkDatum');
+let sixth = new BlogPost('Peace','Love','Unity','Brotherhood');
 
 
 console.log(blogger);
@@ -61,12 +63,14 @@ console.log("Added first: ",blogger.add(first));
 console.log("Added second: ",blogger.add(second));
 console.log("Added third: ",blogger.add(third));
 console.log("Added fifth: ",blogger.add(fifth));
+console.log("Added sixth: ",blogger.add(sixth));
+
 console.log("Get contents: ",blogger.get());
 console.log("Get length: ",blogger.len());
 
-console.log("delete one element: ",blogger.delete(1));
+console.log("delete one element: ",blogger.delete(2));
 
-console.log("update blog: ",blogger.update(2,fourth));
+console.log("update blog: ",blogger.update(3,fourth));
 
 console.log("Blog contents: ",blogger);
 
