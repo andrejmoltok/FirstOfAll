fad0979129f09041080acf31c53e9c460bbb047f
Csokán Pál András <csokan.andras87@protonmail.ch>
2022-06-29 18:22:10 +0200

    delete function modositva + egyebek
    Instead of moving element for deletion to the end, it's functional to move it to the front and then use .shift() to remove it from the array.
    
    uj valtozok vannak megadva teszteleshez
    uj logolasok

--- w33/Blog+BlogPost/Blog.ts
+++ w33/Blog+BlogPost/Blog.ts
@@ -25,10 +25,10 @@ class Blog {
 
     public delete(index:number){
         if (index > 0 && index < this._blogposts.length-1){
-            let temp = this._blogposts[index];
-            this._blogposts[index] = this._blogposts[this._blogposts.length-1];
-            this._blogposts[this._blogposts.length-1] = temp;
-            this._blogposts.pop();
+            // let temp = this._blogposts[index];
+            this._blogposts[index] = this._blogposts[0];
+            // this._blogposts[this._blogposts.length-1] = this._blogposts[this._blogposts.length-2];
+            this._blogposts.shift();
         }
         else if (index == 0)
         {
@@ -41,20 +41,34 @@ class Blog {
         return this._blogposts;
     }
 
-    
+    public len(){
+        return this._blogposts.length;
+    }
+
 }
 
 let blogger = new Blog();
 let first = new BlogPost('John Doe','Lorem ipsum','Lorem ipsum dolor sit amet.','2000.05.04.');
-let second = new BlogPost('agag','ahahrsg','agesthetn','rwbe5h53');
-let third = new BlogPost('shrwrhw','w4h35aj6ja','wrha35hezn','rwhbwrhwrn');
-let fourth = new BlogPost('negyedik','negyedik','negyedik','negyedik');
-console.log(blogger);
-console.log(blogger.add(first));
-console.log(blogger.add(second));
-console.log(blogger.add(third));
-//console.log(blogger.get());
-//console.log(blogger.delete(1));
-console.log(blogger.update(1,fourth));
-//console.log(blogger);
+let second = new BlogPost('Tim Urban','Wait but why','A popular long-form, stick-figure-illustrated blog about almost everything.','2010.10.10.');
+let third = new BlogPost('William Turton','One Engineer Is Trying to Get IBM to Reckon With Trump','Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention. When I asked to take his picture outside one of IBM’s New York City offices, he told me that he wasn’t really into the whole organizer profile thing.','2017.03.28.');
+let fourth = new BlogPost('Jack Brown','What"\'s" going on next?','The future is the present which keeps on evolving','2006.06.15');
+let fifth = new BlogPost('CikkIroNeve','CikkCime','CikkTartalma','CikkDatum');
 
+
+console.log(blogger);
+
+console.log("Added first: ",blogger.add(first));
+console.log("Added second: ",blogger.add(second));
+console.log("Added third: ",blogger.add(third));
+console.log("Added fifth: ",blogger.add(fifth));
+console.log("Get contents: ",blogger.get());
+console.log("Get length: ",blogger.len());
+
+console.log("delete one element: ",blogger.delete(1));
+
+console.log("update blog: ",blogger.update(2,fourth));
+
+console.log("Blog contents: ",blogger);
+
+
+console.log("Get length: ",blogger.len());
