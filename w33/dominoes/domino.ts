2322bb791450cbefcfc0a65779594e43779f8a6b
Csokán Pál András <csokan.andras87@protonmail.ch>
2022-07-01 09:53:06 +0200

    Domino sort proba x FAIL

--- w33/dominoes/domino.ts
+++ w33/dominoes/domino.ts
@@ -4,17 +4,11 @@ class Domino {
         this.values = [valueA, valueB];
     }
 
-    read(index:number):void{
-        console.log(this.values[index]);
+    read(index:number):number{
+       return this.values[index];
     }
 
-    sorter(input:any):void{
-        //open domino set and read values
-        //arrange dominoes based on equal values
-        //if One's last equals to Second's first
-        //put them next to eachother
-        
-    }
+    
 }
 
 export {Domino};
--- w33/dominoes/dominoes.ts
+++ w33/dominoes/dominoes.ts
@@ -2,7 +2,7 @@ import {Domino} from "./domino";
 
 
 function initializeDominoes(): Domino[] {
-    let dominoes:any[] = [];
+    let dominoes = [];
     dominoes.push(new Domino(5, 2));
     dominoes.push(new Domino(4, 6));
     dominoes.push(new Domino(1, 5));
@@ -20,27 +20,44 @@ function print(dominoes: Domino[]) {
 
 //sortirozo funkciot kell irni
 //pl.:[5,2],[2,4],[4,6],[6,7],[7,1],[1,5]
-
-function sorter(input:any){
+function sorter(input:any):void{
+    //     //open domino set and read values
+    //     //arrange dominoes based on equal values
+    //     //if One's last equals to Second's first
+    //     //put them next to eachother
+    let temp = new Array();
     for (let i=0; i<=input.length-1; i++){
-            
+        temp[i] = "";
+        if (input[i].read(1) = input[i+1].read(0)) {
+            temp[i] += input[i];
+            }
     }
-}
+    for (let x=0; x<=temp.length-1; x++){
+        console.log(temp[x]);
+    }
+    }
+
 
 let dominoes = initializeDominoes();
-// console.log(dominoes.pop()?.read(1));
-// console.log(dominoes.pop()?.read(0));
-
-//sorter(dominoes);
 
 /** You have the list of Dominoes */
 /** Order them into one snake where the adjacent dominoes have the same numbers on their adjacent sides */
 /** eg: [2, 4], [4, 3], [3, 5] ... */
+//find a match for first item's last value equals first value of second item
 
 //print(dominoes);
 
-console.log((dominoes)[0].read(0));
-console.log((dominoes)[0].read(1));
+//console.log(dominoes.map((v,i) => {console.log(v.values)}));
+
+sorter(dominoes); 
+
+
+
+
+
+//lehetseges sorrend
+// console.log((dominoes)[0]);
+// console.log((dominoes)[4]);
 // console.log((dominoes)[1]);
 // console.log((dominoes)[3]);
 // console.log((dominoes)[5]);
