26f97e2a8ecb4aa3986406b951d5109c6b030e1c
Csokán Pál András <csokan.andras87@protonmail.ch>
2022-07-07 15:15:22 +0200

    Farm class
    listOfAnimals
    breed()
    sell()

---
+++ w33/Farm/Animal.ts
@@ -0,0 +1,42 @@
+'use strict'
+
+class Animal {
+
+    private _hunger: number;
+    private _thirst: number;
+
+    constructor(){
+        this._hunger = 50;
+        this._thirst = 50;
+    }
+
+    public eat(){
+        this._hunger --;
+        return console.log('Animal has eaten.')
+    }
+
+    public drink(){
+        this._thirst --;
+        return console.log('Animal drank well.')
+    }
+
+    public play(){
+        this._hunger ++;
+        this._thirst ++;
+        return console.log('Animal played well.')
+    }
+}
+
+
+export {Animal}
+
+// let kutya = new Animal();
+// let cica = new Animal();
+
+// console.log(kutya);
+// console.log(cica);
+
+// kutya.eat();
+// kutya.drink();
+// kutya.drink();
+// kutya.play();
---
+++ w33/Farm/Farm.ts
@@ -0,0 +1,52 @@
+'use strict'
+
+import {Animal} from './Animal'
+
+class Farm {
+
+    private _listOfAnimals:any = [];
+    private _limit:number;
+
+    constructor(limit:number){
+        this._listOfAnimals = [];
+        this._limit = limit;
+    }
+
+    public breed():any{
+        if (this._listOfAnimals.length !== this._limit){
+            this._listOfAnimals.push(new Animal());
+            console.log("New animal arrived to the Farm!");
+            console.log(this._listOfAnimals);
+        }
+        else if (this._listOfAnimals.length == this._limit)
+        {
+            console.log("No more space available on the Farm!");
+        }
+    }
+
+    public sell(){
+        for (let i=0; i<=this._listOfAnimals.length-1; i++){
+            if (this._listOfAnimals[i]._hunger < 5){
+                this._listOfAnimals.splice(i,1);
+            }
+        }
+    }
+
+    public checkFarm():any{
+        console.log("The Farm has " + this._listOfAnimals.length + ((this._listOfAnimals.length==1)? " animal." :  " animals."));
+        console.log("The farms animal limit is " + this._limit);
+    }
+
+}
+
+let mcdonald = new Farm(5);
+mcdonald.breed();
+mcdonald.breed();
+mcdonald.breed();
+mcdonald.breed();
+mcdonald.breed();
+mcdonald.breed();
+mcdonald.breed();
+mcdonald.breed();
+mcdonald.checkFarm();
+mcdonald.sell();
