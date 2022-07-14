0705ef73eb9e09f80644a38841286759461720f5
Csokán Pál András <csokan.andras87@protonmail.ch>
2022-07-14 09:57:14 +0200

    Added operand/argument in function call
    if operand is '>' it will look for the Max value
    if operand is '<' it will look for the Min value

--- w34a/maximum.js
+++ w34a/maximum.js
@@ -1,17 +1,37 @@
 //find the largest number in the array recursively
 
-var arr = [5,10,22,56,44,9,11,77,34];
-var n = arr.length;
 
-function maxVal(arr){
-    //console.log(arr[n-1]);
-    if (n == 1){
+function minMaxVal(arr,op){
+    if (arr.length == 1){
         return arr[0];
     }
-    else
-    {
-        return Math.max(arr[n-1], maxVal(arr[n-1]));
+    else if (arr.length > 1){
+        //(arr[0] < arr[1]) ? max = arr[1] : max = arr[0];
+        
+        let empty = 0; 
+        /*change '>' to '<', to find minimum value from array
+        ('>' looks for max value, '<' looks for min value)*/
+        if (op == '>'){
+            if (arr[0]  >  arr[1]){
+                empty = arr[0];
+                arr[0] = arr[1];
+                arr[1] = empty;
+           }
+        }
+        else if (op == '<'){
+            if (arr[0] < arr[1]){
+                empty = arr[0];
+                arr[0] = arr[1];
+                arr[1] = empty; 
+            }
+        }
+        
+        arr.shift();
+        minMaxVal(arr, op);
+        return Number(arr);
     }
 }
 
-console.log(maxVal(arr));
+console.log(minMaxVal([5,10,44,77,41,56,99,17], '>'));
+console.log(minMaxVal([9,2,7,4,19,21,11,13,7,35], '<'));
+console.log(minMaxVal([44,11,89,87,100,11,17,91], '>'));
--- w34a/maximum.ts
+++ w34a/maximum.ts
@@ -1,25 +1,37 @@
 //find the largest number in the array recursively
+/*change '>' to '<', to find minimum value from array
+('>' looks for max value, '<' looks for min value)*/
 
-var arr:number[] = [9,2,7,4,19,21,11,13,7,35]; //5,10,44,77,41,56,99,17
-var n:number = arr.length;
-
-function maxVal(arr:Array<number>):any{
+function minMaxVal(arr:Array<number>,op:string):any{
     if (arr.length == 1){
         return arr[0];
     }
     else if (arr.length > 1){
         //(arr[0] < arr[1]) ? max = arr[1] : max = arr[0];
         
-        let empty:number = 0;       
-        if (arr[0]>arr[1]){
-            empty = arr[0];
-            arr[0] = arr[1];
-            arr[1] = empty;
+        let empty = 0; 
+        
+        if (op == '>'){
+            if (arr[0]  >  arr[1]){
+                empty = arr[0];
+                arr[0] = arr[1];
+                arr[1] = empty;
+           }
         }
+        else if (op == '<'){
+            if (arr[0] < arr[1]){
+                empty = arr[0];
+                arr[0] = arr[1];
+                arr[1] = empty; 
+            }
+        }
+        
         arr.shift();
-        maxVal(arr);
+        minMaxVal(arr, op);
         return Number(arr);
     }
 }
 
-console.log(maxVal(arr));
+console.log(minMaxVal([5,10,44,77,41,56,99,17], '>'));
+console.log(minMaxVal([9,2,7,4,19,21,11,13,7,35], '<'));
+console.log(minMaxVal([44,11,89,87,100,11,17,91], '>'));
