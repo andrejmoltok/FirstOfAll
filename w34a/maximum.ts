22f2e5ec866746bb8d286948090a07284c79e29a
Csokán Pál András <csokan.andras87@protonmail.ch>
2022-07-14 09:18:57 +0200

    maxVal() recursive function complete
    uses bubble-sort-like procedure to determine the maximum number in an array of number, but RECURSIVELY

--- w34a/maximum.ts
+++ w34a/maximum.ts
@@ -4,20 +4,21 @@ var arr:number[] = [5,10,44,77,41,56,99,17];
 var n:number = arr.length;
 
 function maxVal(arr:Array<number>):any{
-    // if (arr.length > 0){    
-    //     maxVal(arr);
-    // }
-    var max:number = 0;
     if (arr.length == 1){
         return arr[0];
     }
     else if (arr.length > 1){
-        (arr[0] > arr[1]) ? max = arr[0] : max = arr[1];
-        arr.shift();
-        console.log(arr);
+        //(arr[0] < arr[1]) ? max = arr[1] : max = arr[0];
         
+        let empty:number = 0;       
+        if (arr[0]>arr[1]){
+            empty = arr[0];
+            arr[0] = arr[1];
+            arr[1] = empty;
+        }
+        arr.shift();
         maxVal(arr);
-        return max
+        return arr
     }
 }
 
