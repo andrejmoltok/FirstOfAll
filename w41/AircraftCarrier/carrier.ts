20402e2caa847b6d8cbcbe4b12d62bdffdd6a2d6
Csokán Pál András <csokan.andras87@protonmail.ch>
2022-07-18 10:31:05 +0200

    Fill not working as intended

--- w41/AircraftCarrier/carrier.ts
+++ w41/AircraftCarrier/carrier.ts
@@ -1,4 +1,4 @@
-import { Aircraft } from "./aircraft";
+import {Aircraft} from "./aircraft";
 import {F16} from './f16';
 import {F35} from './f35';
 
@@ -14,12 +14,36 @@ class Carrier {
         this._aircrafts = [];
         this._initAmmo = initAmmo;
         this._HP = HP;
-        this._ammoStorage = 2300;
+        this._ammoStorage = 20;
     }
 
     public add(aircraft:Aircraft) :any {
         this._aircrafts.push(aircraft);
     }
+
+    public fill() :any {
+        //this._aircrafts[0].getMaxAmmo();
+        //add up the sum of all aircraft ammo in the aircrafts array
+
+        let maxAmmunition:number = 0;
+        for (let i=0; i<=this._aircrafts.length-1; i++){
+            maxAmmunition += Array.from(this._aircrafts)[i].getMaxAmmo();
+        }
+        
+        //console.log(maxAmmunition);
+        
+        if (this._ammoStorage === 0) {
+            throw new Error("The ammunition storage is empty, can't refill aircrafts!");
+        }
+        // else if (this._ammoStorage === maxAmmunition) {
+        //     console.log(Array.from(this._aircrafts).forEach((item) => item.refillAmmo(this._ammoStorage)));
+        // }
+        else {
+            console.log(Array.from(this._aircrafts).forEach((item) => item.refillAmmo(this._ammoStorage)));
+        }
+     
+    }
+
 
 }
 
@@ -27,7 +51,11 @@ let nimitz = new Carrier(100,300);
 let enola = new F35();
 let carl = new F16();
 
+enola.fight(carl);
+
 nimitz.add(enola);
 nimitz.add(carl);
 
+console.log(nimitz);
+console.log(nimitz.fill());
 console.log(nimitz);
