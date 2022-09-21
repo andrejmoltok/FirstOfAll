6cc5200cd4f062b758551f766f6beb2fb3fe8571
Csokán Pál András <csokan.andras87@protonmail.ch>
2022-09-21 19:30:45 +0200

    classlist exercise solved

---
+++ dom/classlist-1/classlist-1.js
@@ -0,0 +1,23 @@
+var allPara = Array.from(document.getElementsByTagName('p'));
+console.log(allPara[3].hasAttribute("dolphin"));
+if (allPara[3].hasAttribute("dolphin")) {
+    document.getElementsByClassName('apple')[0].textContent = "pear";
+}
+console.log(allPara[0].className == "apple");
+if (allPara[0].classname == "apple") {
+    for (let i = 0; i < allPara.length; i++) {
+        if (allPara[i].className == "cat") {
+            allPara[i].textContent = "dog";
+        }
+    }
+}
+
+console.log(document.getElementsByClassName('cat')[0]);
+
+document.getElementsByClassName('apple')[0].setAttribute("class","red");
+
+console.log(allPara);
+
+document.getElementsByClassName('balloon')[0].setAttribute("style","border-radius:20%;");
+
+console.log(allPara);
