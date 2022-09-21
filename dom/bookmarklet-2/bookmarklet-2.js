781f5221ba200b469eeb112bdaf04133ec8dec80
Csokán Pál András <csokan.andras87@protonmail.ch>
2022-09-21 18:02:51 +0200

    bookmarklet-2 solved - img source change

---
+++ dom/bookmarklet-2/bookmarklet-2.js
@@ -0,0 +1,9 @@
+function imgChanger() {
+    var anyImg = document.querySelectorAll('img');
+    anyImg.forEach((change) => {change.src = "http://bit.ly/lpgreenfox"});
+}
+
+function allImgChanger() {
+    var allImg = window.document.querySelectorAll('img');
+    allImg.forEach((all) => {all.src = "http://bit.ly/lpgreenfox"});
+}
