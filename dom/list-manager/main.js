68ac0baeb8d7e59e3f5dab4d273892fa6b1c1a5e
Csokán Pál András <csokan.andras87@protonmail.ch>
2022-09-29 22:47:24 +0200

    majdnem teljes kód újraírás

--- dom/list-manager/main.js
+++ dom/list-manager/main.js
@@ -1,246 +1,61 @@
-//variable definition section
-var container = document.createElement('div');
+// names for three tables to create
+var mainData = "start,func,end".split(',');
 
-var leftBox = document.createElement('div');
-var buttonBox = document.createElement('div');
-var rightBox = document.createElement('div');
+// main DIV container for three tables
+var div = document.createElement('DIV');
+div.setAttribute('id','container');
 
-//buttonUp
-var buttonUp = document.createElement('input');
-buttonUp.setAttribute('type','button');
-buttonUp.value = "Up";
-
-//buttonAdd
-var buttonAdd = document.createElement('input');
-buttonAdd.setAttribute('type','button');
-buttonAdd.value = ">";
-
-//buttonRemove
-var buttonRemove = document.createElement('input');
-buttonRemove.setAttribute('type','button');
-buttonRemove.value = "X";
-
-//buttonDown
-var buttonDown = document.createElement('input');
-buttonDown.setAttribute('type','button');
-buttonDown.value = "Down";
-
-//items
-const items = ['bread','milk','orange','tomato'];
-
-//isClicked empty string
-var isClicked = "";
-
-
-//appendChild section
-document.body.appendChild(container);
-
-container.appendChild(leftBox);
-container.appendChild(buttonBox);
-container.appendChild(rightBox);
-
-buttonBox.appendChild(buttonUp);
-buttonBox.appendChild(buttonAdd);
-buttonBox.appendChild(buttonRemove);
-buttonBox.appendChild(buttonDown);
-
-
-//setAttribute section
-container.setAttribute('id','container');
-leftBox.setAttribute('id','leftBox');
-buttonBox.setAttribute('id','buttonBox');
-rightBox.setAttribute('id','rightBox');
-
-
-//leftBox section
-items.forEach((elem) => {
-    var lBdiv = document.createElement('div');
-    lBdiv.innerText = elem;
-    lBdiv.setAttribute('id',elem + 'Left');
-    lBdiv.style.width = "127px";
-    lBdiv.style.height = "31.5px";
-    leftBox.appendChild(lBdiv);
+// forEach for mainData array to generate tables
+mainData.forEach((v) => {
+    var x = document.createElement("TABLE");
+    x.setAttribute("id", v + "Table");
+    div.appendChild(x);
+    document.body.appendChild(div);
 });
 
-console.log(leftBox.children);
+// startFunction to create rows inside each of the mainData tables
+function startFunction(item) {
+    var z = document.createElement("TR");
+    var t = document.createTextNode(item);
+    z.setAttribute("id", "start" + item);
+    z.appendChild(t);
+    document.getElementById("startTable").appendChild(z);
+}
 
-//eventlistener for leftBox
-leftBox.addEventListener("click", function(event) {
-    breadLeft.style.backgroundColor = "white";
-    milkLeft.style.backgroundColor = "white";
-    orangeLeft.style.backgroundColor = "white";
-    tomatoLeft.style.backgroundColor = "white";
-    if (event.target && event.target.matches('div#breadLeft')) {
-        isClicked = "breadLeft";
-        console.log("BreadLeft Clicked");
-        breadLeft.style.backgroundColor = "lightgray";
-        breadLeft.style.cursor = "default";
-        return isClicked;
-    }
-    if (event.target && event.target.matches('div#milkLeft')) {
-        isClicked = "milkLeft";
-        console.log("MilkLeft Clicked");
-        milkLeft.style.backgroundColor = "lightgray";
-        milkLeft.style.cursor = "default";
-        return isClicked;
-    }
-    if (event.target && event.target.matches('div#orangeLeft')) {
-        isClicked = "orangeLeft";
-        console.log("OrangeLeft Clicked");
-        orangeLeft.style.backgroundColor = "lightgray";
-        orangeLeft.style.cursor = "default";
-        return isClicked;
-    }
-    if (event.target && event.target.matches('div#tomatoLeft')) {
-        isClicked = "tomatoLeft";
-        console.log("TomatoLeft Clicked");
-        tomatoLeft.style.backgroundColor = "lightgray";
-        tomatoLeft.style.cursor = "default";
-        return isClicked;
-    }
+//funcFunction to create functional buttons to move list items
+function funcFunction(item) {
+    var z = document.createElement("TR");
+    var t = document.createTextNode(item);
+    z.setAttribute("id", "func" + item);
+    z.appendChild(t);
+    document.getElementById("funcTable").appendChild(z);
+}
+
+// endFunction to create empty table for future content
+function endFunction(item) {
+    var z = document.createElement("TR");
+    var t = document.createTextNode(item);
+    z.setAttribute("id", "endTr");
+    z.appendChild(t);
+    document.getElementById("endTable").appendChild(z);
+}
+
+// lists of content going inside of mainData tables
+var startList = "bread,milk,orange,tomato".split(',');
+var functionList = "Up,>>,<<,Down".split(',');
+var endList = "' ',' ',' ',' '".split(',');
+
+// forEach function for startList content
+startList.forEach((v) => {
+    startFunction(v);
 });
 
-
-//eventlistener for rightBox
-rightBox.addEventListener("click", function(e) {
-    if (e.target && e.target.matches("#breadRight")) {
-        isClicked = "breadRight";
-        console.log("BreadRight Clicked");
-        breadRight.style.backgroundColor = "lightgray";
-        breadRight.style.cursor = "default";
-        return isClicked;
-    }
-    if (e.target && e.target.matches("#milkRight")) {
-        isClicked = "milkRight";
-        console.log("MilkRight Clicked");
-        milkRight.style.backgroundColor = "lightgray";
-        milkRight.style.cursor = "default";
-        return isClicked;
-    }
-    if (e.target && e.target.matches("#orangeRight")) {
-        isClicked = "orangeRight";
-        console.log("OrangeRight Clicked");
-        orangeRight.style.backgroundColor = "lightgray";
-        orangeRight.style.cursor = "default";
-        return isClicked;
-    }
-    if (e.target && e.target.matches("#tomatoRight")) {
-        isClicked = "tomatoRight";
-        console.log("TomatoRight Clicked");
-        tomatoRight.style.backgroundColor = "lightgray";
-        tomatoRight.style.cursor = "default";
-        return isClicked;
-    }
+// forEach function for functionList content
+functionList.forEach((v) => {
+    funcFunction(v);
 });
 
-//buttonBox section
-
-//buttonAdd
-buttonAdd.addEventListener('click',function() {    
-    if (isClicked === "breadLeft") {
-        breadLeft.remove();
-        var breadDiv = document.createElement('div');
-        leftBox.style.justifyContent = "flex-start";
-        breadDiv.innerText = "bread";
-        breadDiv.setAttribute('id', "breadRight");
-        breadDiv.style.width = "127px";
-        breadDiv.style.height = "31.5px";
-        rightBox.appendChild(breadDiv);
-        isClicked = "";
-        console.log("bread added to rightBox");
-    }
-    if (isClicked === "milkLeft") {
-        milkLeft.remove();
-        var milkDiv = document.createElement('div');
-        leftBox.style.justifyContent = "flex-start";
-        milkDiv.innerText = "milk";
-        milkDiv.setAttribute('id', "milkRight");
-        milkDiv.style.width = "127px";
-        milkDiv.style.height = "31.5px";
-        rightBox.appendChild(milkDiv);
-        isClicked = "";
-        console.log("milk added to rightBox");
-    }
-    if (isClicked === "orangeLeft") {
-        orangeLeft.remove();
-        var orangeDiv = document.createElement('div');
-        leftBox.style.justifyContent = "flex-start";
-        orangeDiv.innerText = "orange";
-        orangeDiv.setAttribute('id', "orangeRight");
-        orangeDiv.style.width = "127px";
-        orangeDiv.style.height = "31.5px";
-        rightBox.appendChild(orangeDiv);
-        isClicked = "";
-        console.log("orange added to rightBox");
-    }
-    if (isClicked === "tomatoLeft") {
-        tomatoLeft.remove();
-        var tomatoDiv = document.createElement('div');
-        leftBox.style.justifyContent = "flex-start";
-        tomatoDiv.innerText = "tomato";
-        tomatoDiv.setAttribute('id', "tomatoRight");
-        tomatoDiv.style.width = "127px";
-        tomatoDiv.style.height = "31.5px";
-        rightBox.appendChild(tomatoDiv);
-        isClicked = "";
-        console.log("tomato added to rightBox");
-    }
+// forEach function for endList future content
+endList.forEach((v) => {
+    endFunction(v);
 });
-
-//buttonRemove
-buttonRemove.addEventListener('click',function() {    
-    if (isClicked === "breadRight") {
-        breadRight.remove();
-        var breadDiv = document.createElement('div');
-        rightBox.style.justifyContent = "flex-start";
-        breadDiv.innerText = "milk";
-        breadDiv.setAttribute('id', "milkLeft");
-        breadDiv.style.width = "127px";
-        breadDiv.style.height = "31.5px";
-        leftBox.appendChild(breadDiv);
-        isClicked = "";
-        console.log("bread removed from rightBox");
-    }
-    if (isClicked === "milkRight") {
-        milkRight.remove();
-        var milkDiv = document.createElement('div');
-        rightBox.style.justifyContent = "flex-start";
-        milkDiv.innerText = "milk";
-        milkDiv.setAttribute('id', "milkLeft");
-        milkDiv.style.width = "127px";
-        milkDiv.style.height = "31.5px";
-        leftBox.appendChild(milkDiv);
-        isClicked = "";
-        console.log("milk removed from rightBox");
-    }
-    if (isClicked === "orangeRight") {
-        orangeRight.remove();
-        var orangeDiv = document.createElement('div');
-        rightBox.style.justifyContent = "flex-start";
-        orangeDiv.innerText = "orange";
-        orangeDiv.setAttribute('id', "orangeLeft");
-        orangeDiv.style.width = "127px";
-        orangeDiv.style.height = "31.5px";
-        leftBox.appendChild(orangeDiv);
-        isClicked = "";
-        console.log("orange removed from rightBox");
-    }
-    if (isClicked === "tomatoRight") {
-        tomatoRight.remove();
-        var tomatoDiv = document.createElement('div');
-        leftBox.appendChild(tomatoDiv);
-        tomatoDiv.setAttribute('id', "tomatoLeft");
-        rightBox.style.justifyContent = "flex-start";
-        tomatoDiv.innerText = "tomato";
-        tomatoDiv.style.width = "127px";
-        tomatoDiv.style.height = "31.5px";
-        isClicked = "";
-        console.log("tomato removed from rightBox");
-    }
-});
-
-// buttonDown.addEventListener("click", function() {
-//     if (isClicked == "breadRight") {
-//         breadRight.parentNode.insertBefore(breadRight, breadRight.nextSibling);
-//     }
-// });
